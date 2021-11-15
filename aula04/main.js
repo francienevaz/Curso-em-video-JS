675a58e53a776a0896acc8f9cdeb5b924960986f
francienevaz <fran.vfraga@gmail.com>
2021-11-15 19:47:56 -0300

    adicionando novas aulas

---
+++ aula04/main.js
@@ -0,0 +1,11 @@
+var nome = "Franciene Vaz";
+var idade= 24
+var idade2 = 35
+var frase = "Testando"
+
+
+//alert ('Bem vindo ' + nome);
+
+console.log(nome);
+console.log(idade);
+console.log(frase.toUpperCase());
